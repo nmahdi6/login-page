be68ba074c09095d85283660c62ee37cbfbf4c92
nmahdi6 <nmahdi600@gmail.com>
2022-09-01 23:06:19 +0430

    add box shodow

--- lib/screen/widget/TextFieldwidget.dart
+++ lib/screen/widget/TextFieldwidget.dart
@@ -18,38 +18,53 @@ class TextFieldWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      validator: validator,
-      enabled: enabled,
-      style: const TextStyle(fontSize: 14, color: Colors.black),
-      obscureText: obscureText,
-      decoration: InputDecoration(
-        labelText: labelText,
-        labelStyle: const TextStyle(color:  Color.fromARGB(255, 132, 132, 132)),
-        filled: true,
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: Colors.white,
+         boxShadow: [
+      BoxShadow(
+        color: Colors.grey.withOpacity(0.5),
+        spreadRadius: 0.3,
+        blurRadius: 1,
+        offset: const Offset(0, 2), // changes position of shadow
+      ),
+    ],
+      ),
+      child: TextFormField(
         
-        enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(20),
-            borderSide: BorderSide.none),
-
-        focusedErrorBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(20),
-            borderSide: BorderSide.none),
-        errorBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(20),
-            borderSide: BorderSide.none),
-        focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(20),
-            borderSide: const BorderSide(color: Color.fromARGB(255, 132, 132, 132))),
-        prefixIcon: Icon(
-          icon,
-          size: 20,
-          color: Colors.black,
-        ),
-        suffixIcon: Icon(
-          suffixIcon,
-          size: 20,
-          color:  Color.fromARGB(255, 132, 132, 132)
+        validator: validator,
+        enabled: enabled,
+        style: const TextStyle(fontSize: 14, color: Colors.black),
+        obscureText: obscureText,
+        decoration: InputDecoration(
+          labelText: labelText,
+          labelStyle: const TextStyle(color:  Color.fromARGB(255, 132, 132, 132)),
+          filled: true,
+          
+          enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20),
+              borderSide: BorderSide.none),
+    
+          focusedErrorBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20),
+              borderSide: BorderSide.none),
+          errorBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20),
+              borderSide: BorderSide.none),
+          focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20),
+              borderSide: const BorderSide(color: Color.fromARGB(255, 132, 132, 132))),
+          prefixIcon: Icon(
+            icon,
+            size: 20,
+            color: Colors.black,
+          ),
+          suffixIcon: Icon(
+            suffixIcon,
+            size: 20,
+            color:  Color.fromARGB(255, 132, 132, 132)
+          ),
         ),
       ),
     );
